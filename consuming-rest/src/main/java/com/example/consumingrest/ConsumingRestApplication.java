feab77d9362ee8ed0b7ffee2f1f90c2280d06055
Robert <20748700+robdoescode@users.noreply.github.com>
2021-04-16 10:01:06 -0500

    fleshed out ConsumingRestApplication.java

--- consuming-rest/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
+++ consuming-rest/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -1,13 +1,36 @@
 package com.example.consumingrest;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConsumingRestApplication {
+
+	// The Logger sends output to the log (the console, in this example)
+	private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumingRestApplication.class, args);
 	}
 
+	// The RestTemplate uses the Jackson JSON processing library to process the incoming data
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	// The CommandLineRunner runs the RestTemplate on startup
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject("https://quoters.apps.pcfone.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
 }
