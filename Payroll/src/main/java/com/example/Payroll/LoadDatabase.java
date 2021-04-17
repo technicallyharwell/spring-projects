1d10b193ad4ca2c672c0cc05dc777b648be95def
Robert <20748700+robdoescode@users.noreply.github.com>
2021-04-17 13:51:27 -0500

    fleshed out LoadDatabase.java

--- Payroll/src/main/java/com/example/Payroll/LoadDatabase.java
+++ Payroll/src/main/java/com/example/Payroll/LoadDatabase.java
@@ -1,4 +1,23 @@
 package com.example.Payroll;
 
-public class LoadDatabase {
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+        return args-> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+        };
+    }
 }
