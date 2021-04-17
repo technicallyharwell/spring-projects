89e164bea174d0a3eb77338be77029e52ed9989b
Robert <20748700+robdoescode@users.noreply.github.com>
2021-04-17 15:26:19 -0500

    fleshed out EmployeeController.java

--- Payroll/src/main/java/com/example/Payroll/EmployeeController.java
+++ Payroll/src/main/java/com/example/Payroll/EmployeeController.java
@@ -1,4 +1,54 @@
 package com.example.Payroll;
 
-public class EmployeeController {
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+class EmployeeController {
+
+    private final EmployeeRepository repository;
+
+    EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    // Aggregate root
+    // tag::get-aggregate-root[]
+    @GetMapping("/employees")
+    List<Employee> all() {
+        return repository.findAll();
+    }
+    // end::get-aggregate-root[]
+
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
+
+    // Single item
+    @GetMapping("/employees/{id}")
+    Employee one(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+    }
+
+    @PutMapping("/employees/{id}")
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newEmployee.setId(id);
+                    return repository.save(newEmployee);
+                });
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
