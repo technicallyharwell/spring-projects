22c7f5c015f4855a18732207f9592d7ea65af9f7
Robert <20748700+robdoescode@users.noreply.github.com>
2021-04-17 18:10:28 -0500

    added comments to annotations

--- Payroll/src/main/java/com/example/Payroll/EmployeeNotFoundAdvice.java
+++ Payroll/src/main/java/com/example/Payroll/EmployeeNotFoundAdvice.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class EmployeeNotFoundAdvice {
 
+    // signals that this advice is rendered straight into the response body
     @ResponseBody
+    // configures the advice to only respond if an EmployeeNotFoundException is thrown
     @ExceptionHandler(EmployeeNotFoundException.class)
+    // says to issue a 404 NOT_FOUND
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String employeeNotFoundHandler(EmployeeNotFoundException ex) {
         return ex.getMessage();
