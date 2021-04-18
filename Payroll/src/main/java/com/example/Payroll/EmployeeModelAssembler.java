fccc8ba5c56a1c779a3a385a6be108ee8a5d0039
Robert <20748700+robdoescode@users.noreply.github.com>
2021-04-18 11:43:24 -0500

    added comments

--- Payroll/src/main/java/com/example/Payroll/EmployeeModelAssembler.java
+++ Payroll/src/main/java/com/example/Payroll/EmployeeModelAssembler.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+// By applying @Component annotation, the assembler will be automatically created when the app starts
 @Component
 class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
 
     @Override
+    // converts a non-model Employee object to a model-based object
     public EntityModel<Employee> toModel(Employee employee) {
         return EntityModel.of(employee,
                 linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
