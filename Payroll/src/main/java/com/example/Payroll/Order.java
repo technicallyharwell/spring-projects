85d11ab935c5a9c8057865d046231f4cbb485de4
Robert <20748700+robdoescode@users.noreply.github.com>
2021-04-18 20:43:01 -0500

    fleshed out Order.java - added getter/setter methods and Override annotations for equals, hashCode, toString

--- Payroll/src/main/java/com/example/Payroll/Order.java
+++ Payroll/src/main/java/com/example/Payroll/Order.java
@@ -8,6 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+// JPA annotation to change the table name to CUSTOMER_ORDER, since ORDER is not a valid name
 @Table(name = "CUSTOMER_ORDER")
 public class Order {
 
@@ -19,6 +20,53 @@ public class Order {
     Order() {}
 
     Order(String description, Status status) {
-        
+        this.description = description;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Order))
+            return false;
+        Order order = (Order) o;
+        return Objects.equals(this.id, order.id) && Objects.equals(this.description, order.description)
+                && this.status == order.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.description, this.status);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "id=" + this.id + ", description='" + this.description + '\'' + ", status=" + this.status
+                + '}';
     }
 }
